ce94e29b268554c19417db86b357ae746d18b969
Alonso Zamorano <alonso.zamorano97@gmail.com>
2020-12-16 10:56:30 -0300

    Added or, zeroOrMore and bind

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,11 @@ import * as Parser from "./parser"
 let source = new Parser.Source("hello1 bye2", 0);
 let result = Parser.Parser.regexp(/hello[0-9]/y).parse(source);
 console.log(result);
+
+
+let pair =
+    Parser.Parser.regexp(/[0-9]+/y).bind((first) =>
+        Parser.Parser.regexp(/,/y).bind((_) =>
+            Parser.Parser.regexp(/[0-9]+/y).bind((second) =>
+                Parser.Parser.constant([first, second]))));
+console.log(pair.parse(new Parser.Source("12,34", 0)))
--- src/parser.ts
+++ src/parser.ts
@@ -8,27 +8,71 @@ export class Source {
     constructor(public string: string, public index: number) {}
 
     match(regexp: RegExp): (ParseResult<string> | null) {
-        console.assert((regexp as any).sticky);
-        regexp.lastIndex = this.index;
-        let match = this.string.match(regexp);
+        console.assert((regexp as any).sticky)
+        regexp.lastIndex = this.index
+        let match = this.string.match(regexp)
         if (match) {
-            let value = match[0];
-            let newIndex = this.index + value.length;
-            let source = new Source(this.string, newIndex);
-            return new ParseResult(value, source);
+            let value = match[0]
+            let newIndex = this.index + value.length
+            let source = new Source(this.string, newIndex)
+            return new ParseResult(value, source)
         }
         return null;
     }
 }
 
-// export interface Parser<T> {
-//     parse(Source): ParseResult<T> | null;
-// }
-
 export class Parser<T> {
     constructor(public parse: (Source) => (ParseResult<T> | null)) {}
 
     static regexp(regexp: RegExp): Parser<string> {
-        return new Parser(source => source.match(regexp));
+        return new Parser(source => source.match(regexp))
+    }
+
+    static constant<U>(value: U): Parser<U> {
+        return new Parser(source => new ParseResult(value, source))
+    }
+
+    static error<U>(message: string): Parser<U> {
+        return new Parser(source => {
+            throw Error(message)
+        })
+    }
+
+    or(parser: Parser<T>): Parser<T> {
+        return new Parser((source) => {
+            let result = this.parse(source)
+            if (result) {
+                return result;
+            }
+            else {
+                return parser.parse(source);
+            }
+        })
+    }
+
+    zeroOrMore<U>(parser: Parser<U>): Parser<Array<U>> {
+        return new Parser(source => {
+            let results = []
+            let item
+            while (item = parser.parse(source)) {
+                source = item.source
+                results.push(item.value)
+            }
+            return new ParseResult(results, source)
+        })
+    }
+
+    bind<U>(callback: (value: T) => Parser<U>): Parser<U> {
+        return new Parser((source) => {
+            let result = this.parse(source)
+            if (result) {
+                let value = result.value
+                let source = result.source
+                return callback(value).parse(source)
+            }
+            else {
+                return null
+            }
+        })
     }
 }
